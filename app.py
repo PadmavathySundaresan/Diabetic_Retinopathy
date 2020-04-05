657691a7cbfd8f6ff29e71a38e93a57f89966b1c
PadmavathySundaresan <62243192+PadmavathySundaresan@users.noreply.github.com>
2020-04-05 21:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ global model
 tf.keras.models.load_model('model.h5')
 print("MODEL LOADED!!!")
 
+@app.route('/')
+def home():
+    return render_template('html.html')
+
 @app.route("/predict", methods=["POST"])
 def doPrediction():
     print("GOT REQUEST")
